afebbae9cee99c56aba0ed86c7e343b08efb9e27
A-hu <louis0911317399@gmail.com>
2019-01-10 17:25:18 +0800

    Route Parameters

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.get('/api/courses', (req, res) => {
   res.send([1, 2,3 ]);
 });
 
+app.get('/api/posts/:year/:month', (req, res) => {
+  // res.send(req.params); // /api/posts/2019/1, output: { year: "2019", month: "1" }
+  res.send(req.query); // /api/posts/2019/1?sortBy=name, output: { sortBy: "name" }
+});
+
 // PORT: export PORT=xxxx in terminal
 const port = process.env.PORT || 3001;
 app.listen(port, () => { console.log(`Listening on port ${port}...`); });
