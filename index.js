47a82ae928b8dea5d95d0873c8c1f55e00bfe280
A-hu <louis0911317399@gmail.com>
2019-01-10 17:32:50 +0800

    Handing HTTP GET Requests

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+  { id: 1, name: 'course1'},
+  { id: 2, name: 'course2'},
+  { id: 3, name: 'course3'},
+];
 
 app.get('/', (req, res) => {
   res.send('Hello Express');
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send([1, 2,3 ]);
+  res.send(courses);
 });
 
-app.get('/api/posts/:year/:month', (req, res) => {
-  // res.send(req.params); // /api/posts/2019/1, output: { year: "2019", month: "1" }
-  res.send(req.query); // /api/posts/2019/1?sortBy=name, output: { sortBy: "name" }
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found.');
+  res.send(course);
 });
 
 // PORT: export PORT=xxxx in terminal
